6111f35e8aa0ca9812f74128cb4eba0b046714d5
Queennora <cstephnora@yahoo.com>
2022-09-20 11:23:27 -0700

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ * Return: string
+ */
+void _puts(char *str)
+{
+	int 1;
+
+	for (1 = 0; str[1] != '\0'; 1++)
+	{
+		_putchar(str[1];
+	}
+	_putchar('\n');
+}
