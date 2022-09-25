1338c851074a25f6cb6fd9f012eaad9f573af4d9
Queennora <cstephnora@yahoo.com>
2022-09-25 07:57:25 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper- Detects upper case letters
+ * @c: Letters to b evaluated
+ *
+ * Return: Always 0on success
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
