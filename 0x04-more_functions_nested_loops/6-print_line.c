ed54a82dbb4f9a39d0f58549570b1da0547c8394
Queennora <cstephnora@yahoo.com>
2022-09-25 09:00:45 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line- To print a line
+ * @n: Number to b evaluated
+ */
+
+void print_line(int n)
+{
+int i = 1;
+while (i <= n)
+{
+_putchar(95);
+i++;
+}
+_putchar('\n');
+}
