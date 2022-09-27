090f645902cc6cf6d14a8f1a0f1f1ab2bec417c0
Queennora <cstephnora@yahoo.com>
2022-09-27 14:30:07 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,17 +1,23 @@
 #include "main.h"
-
 /**
- * _puts- Prints strings
- * @str: String to be used
+ *_strcmp - compares two strings.
+ *@s1: pointer to first string.
+ *@s2: pointer to second string.
+ *
+ *Return: value less than 0 if string is less than the other.
+ *value greater than 0 if string is greater than the other.
+ *0 if strings are equal.
  */
-
-void _puts(char *str)
+int _strcmp(char *s1, char *s2)
 {
-	long p;
+	int counter, cmpVal;
 
-	p = 0;
+	counter = 0;
+	while (s1[counter] == s2[counter] && s1[counter] != '\0')
+	{
+		counter++;
+	}
 
-	for ( ; str[p] != '\0'; p++)
-		_putchar(str[p]);
-	_putchar('\n');
+	cmpVal = s1[counter] - s2[counter];
+	return (cmpVal);
 }
