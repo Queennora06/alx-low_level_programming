f8328eab8134dd96eecb995ef71a8af59e6b70d0
Queennora <cstephnora@yahoo.com>
2022-09-28 13:47:32 -0700

    Task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,20 +1,6 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - size
- * @s: pointer to string params
- * Return: recursion
- */
-int _strlen_recursion(char *s)
-{
-	if (!*s)
-	{
-		return (0);
-	}
-	return (1 + _strlen_recursion(++s));
-}
-
-/**
  * p1 - palindrome
  * @s: pointer to string
  * @1: position
