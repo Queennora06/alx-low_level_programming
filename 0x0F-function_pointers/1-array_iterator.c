108b63d335e29483e78d745582c44ae8ea2bc893
Queennora <cstephnora@yahoo.com>
2022-10-17 05:39:02 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,8 +2,8 @@
 
 /**
  * array_iterator - executes a function given as a parameter on each element of an array.
- * @array: input integer array.
- * @size: size of the array.
+ * @a: input integer array.
+ * @s: size of the array.
  * @action: pointer to the function.
  *
  * Return: no return.
