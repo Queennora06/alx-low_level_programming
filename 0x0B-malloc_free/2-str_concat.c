8a00bec9b60d232f000adffa8e305a1740088075
Queennora <cstephnora@yahoo.com>
2022-10-17 08:48:55 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,36 +1,56 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * create_array - array for prints a string
- * @size: number elements array
- * @c: char
- * Return: pointer
+  *_strlen - counts and returns string length
+  * @s: that's the string
+  *
+  * Return: the length
+  */
+int _strlen(char *s)
+{
+int counter = 0;
+if (!*s)
+return (0);
+while (*s)
+{
+counter++;
+s++;
+}
+return (counter);
+}
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: one string
+ * @s2: the other
+ * Return: pointer to cat string
  */
 
-char *create_array(unsigned int size, char c)
+char *str_concat(char *s1, char *s2)
 {
-	char *buffer;
-	unsigned int position;
-
-	if (size == 0)
-	{
-		return (NULL);
-	}
-
-	/*Define values with mallocs*/
-	buffer = (char *) malloc(size * sizeof(c));
-	if (buffer == 0)
-	{
-		return (NULL);
-	}
-	else
-	{
-		position = 0;
-		while (position < size) /*while for array*/
-		{
-			*(buffer + position) = c;
-			position++;
-		}
-		return (buffer);
-	}
+        char *new;
+        unsigned int i;
+        unsigned int j;
+        int total = 0;
+        if (!s1)
+                s1 = "";
+        if (!s2)
+                s2 = "";
+        total += _strlen(s1) + _strlen(s2);
+        new = malloc((total * sizeof(char)) + 1);
+        if (new == NULL)
+        {
+                return (NULL);
+        }
+        for (i = 0; s1[i]; i++)
+        {
+                new[i] = s1[i];
+        }
+        for (j = 0; s2[j]; j++, i++)
+        {
+                new[i] = s2[j];
+        }
+        new[i] = '\0';
+        return (new);
 }
